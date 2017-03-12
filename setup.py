03bf19e84bcf32ff6e59e04d752f02c1baeeb9bc
Abestanis <abestanis.gc@gmail.com>
2017-03-12 21:33:12 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='TwistedOAuth2',
+    version='0.1',
+    author='Sebastian Scholz',
+    author_email='abestanis.gc@gmail.com',
+    description='A module that allows implementing OAuth2 with twisted',
+    long_description='A module that allows implementing an OAuth2 authorization and token '
+                     'endpoint with twisted',
+    license='MIT',
+    keywords=['OAuth2', 'twisted'],
+    url='https://github.com/Abestanis/TwistedOAuth2',
+    packages=['oauth2'],
+    install_requires=['twisted']
+)
